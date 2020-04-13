3d497302d6b12fc58d310a6edfb8832670be71c3
Iban <rodorro@hotmail.com>
2020-04-13 16:05:34 +0200

    Hook spects

--- src/pods/hotel-collection/hotel-collection.hook.spec.ts
+++ src/pods/hotel-collection/hotel-collection.hook.spec.ts
@@ -3,6 +3,10 @@ import { useHotelCollection } from './hotel-collection.hook';
 import { mapFromApiToVm } from './hotel-collection.mapper';
 import { mapToCollection } from '../../common/mappers/collection.mapper';
 import * as api from './hotel-collection.api';
+import * as commonMapper from 'common/mappers';
+import * as mapper from './hotel-collection.mapper';
+import { HotelEntityVm } from './hotel-collection.vm';
+import { basePicturesUrl } from 'core/const';
 
 describe('Hotel collection hook tests', () => {
     it('should return an empty array and loadHotelCollection a function when it calls ', () => {
@@ -33,7 +37,7 @@ describe('Hotel collection hook tests', () => {
             highRate: 0,
             hotelId: 0,
             hotelInDestination: false,
-            hotelRating: 0,
+            hotelRating: 3,
             location: {
               latitude: 0,
               longitude: 0,
@@ -56,8 +60,24 @@ describe('Hotel collection hook tests', () => {
           }
         ];
 
+        const hotelsViewModel: HotelEntityVm[] = [
+          {
+            id: "1",
+            picture: `${basePicturesUrl}/picture.jpg`,
+            name: "hotel",
+            description: "description",
+            rating: 3,
+            address: "address",
+          }
+        ];
+
         const getHotelColecctionStub = jest.spyOn(api, 'getHotelCollection').mockResolvedValue(hotels);
-        const hotelsMapped = mapToCollection(hotels, mapFromApiToVm);  
+
+        const mapToCollectionStub = jest.spyOn(commonMapper, 'mapToCollection');
+  
+        const mapFromApiToVmStub = jest.spyOn(mapper, 'mapFromApiToVm');
+
+        // const hotelsMapped = mapToCollection(hotels, mapFromApiToVm);  
 
         // Act
         const { result, waitForNextUpdate } = renderHook(() => useHotelCollection());
@@ -74,6 +94,8 @@ describe('Hotel collection hook tests', () => {
         
         // Assert
         expect(getHotelColecctionStub).toHaveBeenCalled();
-        expect(result.current.hotelCollection).toEqual(hotelsMapped);
+        expect(mapToCollectionStub).toHaveBeenCalled();
+        expect(mapFromApiToVmStub).toHaveBeenCalledTimes(hotels.length);
+        expect(result.current.hotelCollection).toEqual(hotelsViewModel);
     });
 });
